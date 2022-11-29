3a2d9c143a595afae26835027bea145cf5e7709c
Waellan77 <sylvainperez77@gmail.com>
2022-11-29 16:16:19 +0100

    Creation middleware with first object

--- app.js
+++ app.js
@@ -9,4 +9,23 @@ app.use((req, res, next) => {
     next()
 })
 
+app.use('/api/sauces', (req, res, next) => {
+    const sauces = [
+        {
+            _id: '00001',
+            userId: '',
+            name: 'Samouraï',
+            manufacturer: 'Amora',
+            description: `La sauce samouraï est une sauce typiquement belge, assez relevée, servie traditionnellement avec des frites. Elle n'est pas, comme son nom pourrait le faire croire, originaire du Japon. Il s'agit d'un mélange de mayonnaise, de ketchup et de sambal ulek ou de harissa.`,
+            mainPepper: 'Sambal Ulek',
+            imageUrl: '',
+            heat: 7,
+            likes: 100,
+            dislikes: 2,
+            usersLiked: ['likes' + 'userId'],
+            usersDisliked: ['dislikes' + 'userId']
+        }
+    ]
+})
+
 module.exports = app
