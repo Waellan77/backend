f13ae5c93c76e14b0f06ba76c59c3855617d58b1
Waellan77 <sylvainperez77@gmail.com>
2022-12-08 16:36:21 +0100

    creation routes sauces

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-const Sauce = require('./models/sauces')
+const saucesRoutes = require('./routes/sauces')
 
 const app = express()
 
@@ -22,38 +22,6 @@ app.use((req, res, next) => {
     next()
 })
 
-app.post('/api/sauces', (req, res, next) => {
-    delete req.body._id
-    const sauce = new Sauce({
-        ...req.body
-    })
-    sauce.save()
-        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
-        .catch(error => res.status(400).json({ error }))
-})
-
-app.put('/api/sauces/:id', (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
-        .catch(error => res.status(400).json({ error }))
-})
-
-app.delete('/api/sauces/:id', (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
-        .catch(error => res.status(400).json({ error }))
-})
-
-app.get('/api/sauces/:id', (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(404).json({ error }))
-})
-
-app.get('/api/sauces', (req, res, next) => {
-    Sauce.find()
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }))
-})
+app.use('/api/sauces', saucesRoutes)
 
 module.exports = app
