fd7158a55068ece6c05620fb46e9cb1e01088e08
Waellan77 <sylvainperez77@gmail.com>
2022-12-15 16:18:16 +0100

    save userRoutes in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const saucesRoutes = require('./routes/sauces')
+const userRoutes = require('./routes/user')
 
 const app = express()
 
@@ -23,5 +24,6 @@ app.use((req, res, next) => {
 })
 
 app.use('/api/sauces', saucesRoutes)
+app.use('/api/auth', userRoutes)
 
 module.exports = app
